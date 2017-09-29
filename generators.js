a3fd48f9518ef69e9557245df5c15b2b2d4a26c3
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2017-09-29 20:57:51 +0700

    Update generators.js

--- generators.js
+++ generators.js
@@ -393,6 +393,6 @@ function arbitraryCloud(){
 		let rand_y = random(cloud_y + 10, cloud_height - rand_height);
 		ellipse(rand_x, rand_y, rand_width, rand_height);
 	}
-  return [cloud_center.x, cloud_center.y, width - 200, height - 200];
+  return [width / 2 - 2 * cloud_width / 1.4, height / 2 - 2 * cloud_height / 1.4, 4 * cloud_width / 1.4, 2 * cloud_height / 1.4];
 }
 register(arbitraryCloud, "Arbitrary cloud", "Hung Nguyen (fb.com/ZeroXCEH)");
