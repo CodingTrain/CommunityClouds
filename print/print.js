2b0b37fa40a6d4ef9b28a7b8cea60ff5151fcb07
Oliver Wright <meiamsome@meiamso.me>
2017-10-09 16:36:35 +0100

    Print GUI updates

--- print/print.js
+++ print/print.js
@@ -1,14 +1,59 @@
 let canvas;
 let canvasSvg;
 let generators = [];
-let addStrokeInput;
+
+let currentSvg;
+let container;
+let generatorSelect;
+let sizeAdjust;
+let filterSelect;
+let backgroundSelect;
+let addToPage;
+
+let currentPage;
+let pageContainer;
+
+const outputWidth = 1200;
+const outputHeight = 900;
 
 function setup() {
-  canvas = createCanvas(1600,900);
+  container = document.getElementById('svg-contain');
+  sizeAdjust = document.getElementById('size-adjust');
+  generatorSelect = document.getElementById('generator');
+  filterSelect = document.getElementById('filter');
+  backgroundSelect = document.getElementById('background');
+  pageContainer = document.getElementById('pages');
+  addToPage = document.getElementById('add-to-page');
+
+  currentPage = new_page();
+
+  let scale = 0.1 + sizeAdjust.value / 100;
+  canvas = createCanvas(1200 * scale, 900 * scale);
   canvas.parent("sketch-contain");
   noLoop();
-  // Change 9 to amount required - 1
-  setTimeout(redraw, 100, 9);
+
+  sizeAdjust.onchange = function() {
+    let scale = 0.1 + sizeAdjust.value / 100;
+    resizeCanvas(1200 * scale, 900 * scale);
+  }
+
+  generators.forEach(function(gen, i) {
+    let elem = document.createElement('option');
+    elem.value = i;
+    elem.innerHTML = gen.name;
+    generatorSelect.appendChild(elem);
+  });
+
+  generatorSelect.onchange = redraw;
+
+  filterSelect.onchange = applyFilter;
+
+  container.className = backgroundSelect.value;
+  backgroundSelect.onchange = function() {
+    container.className = backgroundSelect.value;
+  }
+
+  addToPage.onclick = add_to_page;
 }
 
 function draw() {
@@ -24,49 +69,30 @@ function draw() {
   fill("#000");
   stroke("#000");
   fill("#FFF");
-  var gen = random(generators);
+  gen = generators[generatorSelect.value];
   console.log(generators.indexOf(gen), gen);
-  //gen = generators[9];
   gen.fn();
 
   var xmlns = "http://www.w3.org/2000/svg";
-  var svg = canvasSvg.getSvg();
-  scale_svg(svg, true);
-  svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
-  var g = svg.lastChild;
-  svg.removeChild(g);
+  currentSvg = canvasSvg.getSvg();
+  currentSvg.setAttribute('width', outputWidth);
+  currentSvg.setAttribute('height', outputHeight);
+  scale_svg(currentSvg, true);
+  currentSvg.setAttribute('viewBox', `0 0 ${outputWidth} ${outputHeight}`);
+  var g = currentSvg.lastChild;
+  currentSvg.removeChild(g);
   var container_g = document.createElementNS(xmlns, 'g');
   container_g.appendChild(g);
-  svg.appendChild(container_g);
+  currentSvg.appendChild(container_g);
 
+  applyFilter();
 
-  var div = document.createElement('div');
-  var controls = document.createElement('div');
-  div.appendChild(controls);
-
-  controls.innerHTML = "Add Stroke: ";
-  var stroke_box = document.createElement('input');
-  stroke_box.type = 'checkbox';
-  controls.appendChild(stroke_box);
-
-  controls.appendChild(document.createElement('br'));
-
-  var remove = document.createElement('a');
-  remove.innerHTML = 'Remove';
-  controls.appendChild(remove);
-
-  document.body.appendChild(div);
-  div.appendChild(svg);
-
-  stroke_box.onchange = function() {
-    svg.lastChild.setAttribute('filter', this.checked ? 'url(#stroke)' : '');
+  while(container.lastChild) {
+    container.removeChild(container.lastChild);
   }
-
-  remove.onclick = function() {
-    document.body.removeChild(div);
-    redraw();
-  }
+  container.appendChild(currentSvg);
 }
+
 
 function scale_svg(svg, append, margin) {
   if(append) {
@@ -91,6 +117,49 @@ function scale_svg(svg, append, margin) {
   }
 }
 
+function applyFilter() {
+  if(filterSelect.value !== 'none') {
+    currentSvg.lastChild.setAttribute('filter', `url(#${filterSelect.value})`);
+  } else {
+    currentSvg.lastChild.setAttribute('filter', '');
+  }
+}
+
+function new_page() {
+  let template = document.getElementById('page-template').cloneNode(true);
+  template.id = "";
+  let anchor = document.createElement('a');
+  anchor.target = "_blank";
+  anchor.appendChild(template);
+  pageContainer.appendChild(anchor);
+  let page = {
+    a: anchor,
+    elements: 0,
+    svg: template,
+  };
+  update_link(page);
+  return page;
+}
+
+function update_link(page) {
+  let blob = new Blob([page.svg.outerHTML], {type:"image/svg+xml;charset=utf-8"});
+  let svgUrl   = URL.createObjectURL(blob);
+  page.a.href = svgUrl;
+}
+
+function add_to_page() {
+  let id = currentPage.elements;
+  let offsetX = (id % 2) * outputWidth;
+  let offsetY = Math.floor(id / 2) * outputHeight;
+  let newPart = currentSvg.lastChild.cloneNode(true);
+  currentPage.svg.appendChild(newPart);
+  newPart.setAttribute('transform', `translate(${offsetX}, ${offsetY})`);
+  update_link(currentPage);
+  currentPage.elements += 1;
+  if(currentPage.elements === 6) {
+    currentPage = new_page();
+  }
+}
 
 // Register a new cloud generator.
 function register(fn, name, creator) {
